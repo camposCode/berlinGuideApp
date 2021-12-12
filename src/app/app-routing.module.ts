60dc6bed4b2803d237a4e5eefaee4cf6924ebc1d
camposCode <info@josecampos.io>
2021-12-12 18:45:44 +0100

    pictures and logo uploaded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { MonumentsComponent } from './components/monuments/monuments.component';
+import { MonumentComponent } from './components/monument/monument.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'monuments', component: MonumentsComponent},
+  {path: 'monument/:id', component: MonumentComponent},
+  {path: 'about', component: AboutComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
